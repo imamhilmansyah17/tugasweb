de2be98c6fb4292d9a702a00b4e04777af91f4a6
imamhilmansyah17 <imamhilmansyah@gmail.com>
2017-09-28 22:57:28 +0700

    LEVEL 1
    External Page Submission

---
+++ form_external.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP forms</title>
+</head>
+<body>
+
+<form action="form_process.php" method="post">
+	<input type="text" name="username" placeholder="Enter username" />
+	<input type="password" name="password" placeholder="Enter Password" />
+	<br />
+<input type ="submit" name="submit" />
+</form>
+
+</body>
+</html>
---
+++ form_process.php
@@ -0,0 +1,40 @@
+<! DOCTYPE html>
+<?php
+if(isset($_POST[submit])){
+		$name = array("alexwawo","maria","johndoe","janedoe","nathan");
+	
+		$minimum =5;
+		$maximum =10;
+		
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		
+		if(strlen($username) < 5){
+			echo "Username harus memiliki panjang 5 atau lebih";
+		}
+		if(strlen($username) > $maximum){
+			echo "Username tidak boleh lebih panjang dari 10";
+		}
+		if(!in_array($username, $name)){
+			echo "Maaf, Akses ditolak";
+		}else{
+			echo "selamat datang";
+		}
+
+}	
+?>
+<html>
+<head>
+	<title>PHP Forms</title>
+</head>
+<body>
+
+<form action="form_process.php" method="post">
+	<input type="text" name="username" placeholder="Enter Username" />
+	<input type="password" name="password" placeholder="Enter Password" />
+	<br />
+	<input type="submit" name="submit"/>
+</form>
+
+</body>
+</html>
