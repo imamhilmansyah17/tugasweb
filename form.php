edf4de8f26e432db4322e0e0c47e08d0873ae5a1
imamhilmansyah17 <imamhilmansyah@gmail.com>
2017-09-28 22:37:06 +0700

    Revert "LEVEL 1"
    This reverts commit dac9fadb346fe86e2587b02b4da7557266bac662.

--- form.php
+++ form.php
@@ -1,25 +1,11 @@
 <?php
 if(isset($_POST['submit'])){
-		$name = array("alexwawo","maria","johndoe","janedoe","nathan");
-	
-		$minimum =5;
-		$maximum =10;
-		
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 		
-		if(strlen($username) < 5){
-			echo "Username harus memiliki panjang 5 atau lebih";
-		}
-		if(strlen($username) > $maximum){
-			echo "Username tidak boleh lebih panjang dari 10";
-		}
-		if(!in_array($username, $name)){
-			echo "Maaf, Akses ditolak";
-		}else{
-			echo "selamat datang";
-		}
-
+	echo $username ;
+	echo "<br />";
+	echo $password ;
 }	
 ?>
 <!DOCTYPE html>
@@ -31,7 +17,7 @@ if(isset($_POST['submit'])){
 
 <form action="form.php" method="post">
 	<input type="text" name="username" placeholder="Enter Username" />
-	<input type="password" name="password"	 placeholder="Enter Password" />
+	<input type="password" name="password" placeholder="Enter Password" />
 	<br />
 	<input type="submit" name="submit" />
 </form>
